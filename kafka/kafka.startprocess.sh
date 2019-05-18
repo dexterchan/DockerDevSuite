e59dbc4e8095707221310f16039d8c839910aae7
dexterchan <dexterchan@gmail.com>
2019-05-18 15:25:42 +0800

    add swarm tutorial

--- kafka/kafka.startprocess.sh
+++ kafka/kafka.startprocess.sh
@@ -1,3 +1,5 @@
 bin/zookeeper-server-start.sh config/zookeeper.properties & 
 sleep 10
-bin/kafka-server-start.sh config/server.properties
+bin/kafka-server-start.sh config/server.properties 
+#sleep 5
+#bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
